c2d8cd771294160b014a37099f1bc52b2efd559f
Andrey Helldar <helldar@dragon-code.pro>
2025-02-20 22:21:23 +0300

    Increased search for addictions

--- app/Brands/Brand.php
+++ app/Brands/Brand.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace DragonCode\IconifyIde\Brands;
 
 use DragonCode\IconifyIde\Services\Filesystem;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 abstract class Brand
@@ -45,8 +44,12 @@ abstract class Brand
     protected function search(array $dependencies, array $sections): bool
     {
         foreach ($sections as $section) {
-            if (Arr::hasAny($dependencies[$section] ?? [], $this->projects)) {
-                return true;
+            foreach ($dependencies[$section] ?? [] as $dependency => $version) {
+                foreach ($this->projects as $project) {
+                    if (Str::is($project, $dependency)) {
+                        return true;
+                    }
+                }
             }
         }
 
